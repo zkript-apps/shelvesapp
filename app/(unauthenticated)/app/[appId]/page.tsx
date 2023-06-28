05ef41382efb8c94b097bcf460f81adb2347e225
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-06-28 09:01:22 +0800

    fix: Change env sample

---
+++ app/(unauthenticated)/app/[appId]/page.tsx
@@ -0,0 +1,11 @@
+import AppEdit from "@/components/AppEdit"
+
+export const metadata = {
+    title: 'Shelves - App',
+    description: 'Shelves',
+}
+
+export default function AppPage({ params }: { params: { appId: number } }) {
+    const appId = params.appId;
+    return <></>
+}
