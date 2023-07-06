4ce1ddc60283457c9d8d67c46f5c26230c62ff0a
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-07-06 21:16:10 +0800

    test api

---
+++ pages/api/app/test.ts
@@ -0,0 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from "@prisma/client";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    res.json(req.headers)
+  }
+}
