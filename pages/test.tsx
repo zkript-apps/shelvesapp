ce37f6ad6f0cd3c487b1dfb6dfdc9b65e8482f84
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-07-06 21:08:34 +0800

    test api

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export async function getServerSideProps({ req }: any) {
+  const forwarded = req.headers["X-Real-IP"]
   return {
     props: {
-      ip: req.headers,
+      ip: forwarded,
     },
   }
 }
