3e5e6339b902c0cc00406c603202fd97554eb17f
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-07-06 21:43:01 +0800

    test api

--- pages/api/app/test.ts
+++ pages/api/app/test.ts
@@ -6,7 +6,7 @@ const ipinfoWrapper = new IPinfoWrapper("2e75cee26618a3");
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     const forwarded: string = req.headers["x-forwarded-for"] as string;
-    const ip = forwarded ? forwarded.split(/, /)[0] : "";
+    const ip = forwarded ? forwarded.split(",")[0] : "";
     ipinfoWrapper.lookupIp(ip as string).then((response: IPinfo) => {
       res.json({ response })
     }).catch((e) => {
