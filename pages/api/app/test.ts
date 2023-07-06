ddd2dcd40be79a03877726877de9943352e709c4
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-07-06 20:33:45 +0800

    test api

--- pages/api/app/test.ts
+++ pages/api/app/test.ts
@@ -2,6 +2,6 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
-    res.json(req.headers)
+    res.json(req)
   }
 }
